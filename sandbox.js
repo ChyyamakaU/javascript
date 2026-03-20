8096cf7cc76528777cc330cee646882807bf38a5
ChyyamakaU <amakaumunna90@gmail.com>
2026-03-20 17:22:31 +0100

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,120 @@
+// var myProfile = {
+//     name: "Amaka",
+//     age: 50,
+//     isStudent: false
+
+// };
+
+// console.log(myProfile);
+
+
+// let name = "John",
+// let age = 20;
+
+// console.log(name)
+
+// reason for error: with the keyword let,you can only declare and reassign but not redeclare. For this syntax, it should be age = 20. There is no need for repeating the let keyword, hence the error.
+
+
+console.log(true + false)
+console.log(true + true)
+console.log(false + false)
+
+// I observed that true+false gives 1, true + true, 2, false + false, 0. 
+// This means that js assigns 1 to true and 0 to false
+
+// 4.
+var studentName = "Amaka";
+var studentAge = 20;
+var isGraduated = true;
+
+console.log(studentName)
+console.log(studentAge)
+console.log(isGraduated)
+
+
+// 5.
+var person = {
+    name: "John",
+    age: 25,
+    isStudent: true,
+
+};
+
+console.log(person.name)
+console.log(person.age)
+console.log(person.isStudent)
+
+
+person.age = 39;
+
+console.log(person.age)
+
+person.country = "Nigeria";
+console.log(person.country)
+
+
+
+// 6.
+
+var phone = {
+
+    battery: "4300 MAh approximately",
+    camera: "48 mp",
+    storage: "256g RAM",
+    weight: "240 grams",
+    price: 100000,
+    isAndriod: false,
+    owner: null
+
+};
+
+console.log(phone)
+
+// i didnt notice at first but i wrote the n in null with uppercase and kept getting an error
+
+
+
+// 7. 
+let user ={
+    username:"admin",
+    password: "1234"
+};
+
+console.log(user.username)
+console.log(user.password)
+ 
+
+// 8. 
+
+let name = null;
+let age 
+
+console.log(name)
+console.log(age)
+
+// difference
+// the first is a null data type which means there is no value for name
+// the second is undefined which means it's value is yet to be assigned.
+
+
+// 9.
+var person = {
+name: "Ama",
+age: 12,
+isStudent: true
+
+};
+
+console.log(person)
+
+
+// 10.
+
+let x = null;
+let y  ;
+
+console.log(x)
+console.log(y)
+console.log(typeof x)
+console.log(typeof y)
